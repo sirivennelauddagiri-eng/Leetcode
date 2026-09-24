a2ecf8744f0a34dd78d02f4c11d15cfd63f3faec
siri vennela <sirivennelauddagiri@gmail.com>
2026-09-24 14:26:24 +0530

    Time: 7 ms (47.83%) | Memory: 12.3 MB (61.59%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.py
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.py
@@ -2,9 +2,10 @@ class Solution(object):
     def smallestIndex(self, nums):
         for i in range(len(nums)):
             d = 0
-            while nums[i] > 0:
-                d += nums[i]%10
-                nums[i] //= 10
+            n = nums[i] 
+            while n > 0:
+                d += n%10
+                n //= 10
             if d == i:
                 return i
         return -1
